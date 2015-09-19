fc1913d1256c0dde6956e9dd71aa002824e51ca8
cryptolemming <aliayoub22@gmail.com>
2015-09-19 14:02:27 -0400

    routes and error handling

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var mongoose = require('mongoose');
 var express = require('express');
+var routes = require('./routes');
 
 mongoose.connect('mongodb://localhost', function(err) {
 	if(err) throw err;
 	console.log('connected!');
 
 	var app = express();
-	app.get('/', function(req, res) {
-		res.status(200).send('hello mongoose blog');
-	})
+	routes(app);
+
 	app.listen(3000, function() {
 		console.log('now listening on http://localhost:3000');
 	})
